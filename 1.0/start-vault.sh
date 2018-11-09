4405677a3f80e38c5cd8848319173e0b5264d80f
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-11-09 10:35:21 -0500

    fix chmod issue

--- 1.0/start-vault.sh
+++ 1.0/start-vault.sh
@@ -5,6 +5,9 @@ INSTANCE_NAME=$2
 INSTANCE_IP=$3
 ENV=${4:-fiab}
 
+# Set permissions on rendered config file
+chmod 755 $PWD/vault/vault-config.json
+
 gcloud compute --project ${GOOGLE_PROJ} scp --recurse ./vault root@${INSTANCE_NAME}:/
 gcloud compute --project ${GOOGLE_PROJ} ssh root@${INSTANCE_NAME} --command="LOCAL_IP=${INSTANCE_IP} bash /vault/start-vault.sh"
 export VAULT_ADDR=http://${INSTANCE_IP}:9200
@@ -18,9 +21,6 @@ docker run -v $PWD/vault:/app \
            -e VAULT_TOKEN=unset \
            broadinstitute/dsde-toolbox:latest render-templates.sh
 
-# Set permissions on rendered config file
-chmod 777 $PWD/vault/vault-config.json
-
 # Copy the vault config for later use in rendering allocator configs
 cp vault/vault-config.json allocator
-chmod 777 $PWD/allocator/vault-config.json
+chmod 755 $PWD/allocator/vault-config.json
